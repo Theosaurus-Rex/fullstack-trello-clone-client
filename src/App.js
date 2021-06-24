2b15e64a8a97a2acd9bd833cb92bf33c768f0fe9
Theosaurus-Rex <theosaurus13@gmail.com>
2021-06-24 10:58:04 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
+import { Button, Typography, Box } from '@material-ui/core';
+
+const apiUrl = process.env.REACT_APP_API_URL
 
 function App() {
+
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    axios.get(`${apiUrl}/cards`)
+      .then(({data}) => setCards(data))
+  }, [])
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography>
+          Trello Clone
+        </Typography>
+        <Button>
+          This is a Button
+        </Button>
+        {cards.map(({title, description, id}) => (
+          <Box key={id}>
+            <Typography>{title}</Typography>
+            <Typography>{description}</Typography>
+          </Box>
+        ))}
       </header>
     </div>
   );
