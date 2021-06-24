e8934ad7225c82d7b41fb51d108bd7ee8fa070f2
Theosaurus-Rex <theosaurus13@gmail.com>
2021-06-24 16:45:13 +1000

    fix validator bug

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,10 +1,12 @@
 export const validateInput = (input) => {
     const bannedWords = ["bum"]
+    let valid = true
     bannedWords.forEach((word) => {
-        if (input.includes(word)) {
-            return false
-        } else {
-            return true
-        }
+      if (input.includes(word)) {
+        valid = false
+        return
+      }
     })
-}
+  
+    return valid
+  }
